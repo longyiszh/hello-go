ea5b8baff1ebf4a0e61eb2876abe4ae0da88fb08
longyiszh <longyiszh@gmail.com>
2018-03-24 11:33:04 +0800

    + simple go web server

---
+++ server.go
@@ -0,0 +1,18 @@
+package main
+
+import "fmt"
+import "net/http"
+
+func indexHand(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Go server works!")
+}
+
+func gdhHand(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "gdh works!")
+}
+
+func main() {
+	http.HandleFunc("/", indexHand)
+	http.HandleFunc("/gdh", gdhHand)
+	http.ListenAndServe(":5891", nil)
+}
